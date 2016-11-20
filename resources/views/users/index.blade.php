f6753b67369423973af4651d1eba65e1b4b6c77d
Brett Wilson <brettwilson7@yahoo.com>
2016-11-20 13:02:26 -0500

    Bootstrap panel added to index view and css overrides.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,25 @@
 @include('partials.head')
-        <h1>Users Index</h1>
-        <ul>
-            @foreach ($users as $user)
-                <li>Username: <a href="/users/{{$user->id}}">{{ $user->username }}</a></li>
-            @endforeach
-        </ul><br />
-        <div>
-            <a href="/users/new" class="btn btn-primary">Add User</a>
-            <a href="/" class="btn btn-default">Home</a>
-        </div>
+        <h1>Users Index</h1><br />
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <label class="panel-title">Active Users</label>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group col-md-10 col-md-offset-1">
+                        <ol>
+                            @foreach ($users as $user)
+                                <li class="list-group-item">
+                                    <a href="/users/{{$user->id}}">{{ $user->username }}</a>
+                                </li>
+                            @endforeach
+                        </ol>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <a href="/users/new" class="btn btn-primary">Add User</a>
+                    <a href="/" class="btn btn-default">Home</a>
+                </div>
+            </div>
+        <br />
+
 @include('partials.foot')
