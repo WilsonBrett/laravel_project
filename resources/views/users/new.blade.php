c8ec08255dcffd9924c50896cab05432a7afa15e
Brett Wilson <brettwilson7@yahoo.com>
2016-11-21 16:36:01 -0500

    Placed bootstrap panel inside new user form elements instead of having it outside.

--- resources/views/users/new.blade.php
+++ resources/views/users/new.blade.php
@@ -1,9 +1,9 @@
 @include('partials.head')
         <h1>New User</h1><br />
-        <div class="panel panel-primary">
-            <div class="panel-body">
-                <form method="post" action="/users/create">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <form method="post" action="/users/create">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="panel panel-primary">
+                <div class="panel-body">
                     <div class="form-group">
                         <label for="first_name">First Name</label>
                         <input type="text" name="fname" class="form-control" id="first_name" placeholder="First Name">
@@ -16,12 +16,14 @@
                         <label for="user_name">Username</label>
                         <input type="text" name="uname" class="form-control" id="user_name" placeholder="Username">
                     </div>
+                </div>
+                <div class="panel-footer">
                     <div class="form-group">
                         <input type="submit" name="submit" value="Submit" class="btn btn-primary">
                         <a href="/users" class="btn btn-default">Cancel</a>
                         <a href="/" class="btn btn-default">Home</a>
                     <div>
-                </form>
+                </div>
             </div>
-        </div>
+        </form>
 @include('partials.foot')
