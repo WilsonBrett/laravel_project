1e2b77896167a23a3be9a991c8147de636e15f21
Brett Wilson <brettwilson7@yahoo.com>
2016-11-21 19:04:11 -0500

    Added bootstrap panel and form group styling to edit form.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,11 +3,24 @@
         <form method="post" action="/users/{{ $user[0]->id }}">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             {{ method_field('PUT') }}
-            Firstname: <input name="fname" type="text" value={{ $user[0]->firstname }}><br />
-            Lastname: <input name="lname" type="text" value={{ $user[0]->lastname }}><br />
-            Username: <input name="uname" type="text" value={{ $user[0]->username }}><br /><br />
-            <input name="submit" type="submit" class="btn btn-primary" value="Submit">
-            <a href="/users" class="btn btn-default">Cancel</a>
-        </form><br />
-
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="first_name">First Name</label>
+                        <input name="fname" type="text" id="first_name" class="form-control" value={{ $user[0]->firstname }}>
+                    </div>
+                    <div class="form-group">
+                        <label for="last_name">Last Name</label>
+                        <input name="lname" type="text" id="last_name" class="form-control" value={{ $user[0]->lastname }}>
+                    </div>
+                    <div class="form-group">
+                        <label for="user_name">Username</label>
+                        <input name="uname" type="text" id="user_name" class="form-control" value={{ $user[0]->username }}>
+                    </div>
+                </div>
+            <div class="panel-footer">
+                <input name="submit" type="submit" class="btn btn-primary" value="Submit">
+                <a href="/users" class="btn btn-default">Cancel</a>
+            </div>
+        </form>
 @include('partials.foot')
