bee2c33b38fea0a75fca11ef56ea3874c9f4f2d4
Brett Wilson <brettwilson7@yahoo.com>
2016-12-24 23:02:51 -0500

    Enhanced build process.

--- gulpfile.js
+++ gulpfile.js
@@ -16,21 +16,33 @@ require('laravel-elixir-vue-2');
 /*
     app.scss imports bootstrap stylesheets but not bootstrap fonts or javascripts
         bootstrap fonts and js's are moved to public folder using copy functions below
-    app.scss also imports scss for font awesome
+    app.scss also imports scss for font awesome and my sass styles
     mix.sass resolves all the scss files being imported into app.scss and combines them
         into one file app.css which is placed into public/css
 */
 
 elixir(function(mix) {
-    var bootstrapPath = 'node_modules/bootstrap-sass/assets';
-    var fontAwesomePath = 'vendor/fortawesome/font-awesome';
 
-    mix.sass('app.scss')
-       .copy(bootstrapPath + '/fonts', 'public/fonts')
-       .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js')
-       .copy(fontAwesomePath + '/fonts', 'public/fonts')
-       .copy('vendor/components/jquery/jquery.min.js', 'public/js')
-       .copy('vendor/components/jqueryui/jquery-ui.min.js', 'public/js')
-       .copy('vendor/components/jqueryui/themes/base', 'public/css/base-theme')
-       .webpack('app.js');
+    //resolves all import statements in app.scss and outputs app.css in public/css dir
+    //@TODO:  having trouble using elixir to bring in bootstrap - importing in app.scss works for now.
+    mix.sass(['app.scss',
+              './vendor/components/jqueryui/themes/base/jquery-ui.css'
+    ]);
+
+    //concatenates js files and puts them into in public/js/all.js
+    mix.scripts(['./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+                 './vendor/components/jquery/jquery.js',
+                 './vendor/components/jqueryui/jquery-ui.js',
+                 'main.js'
+    ]);
+
+    //move bootstrap and font-awesome fonts to public fonts dir
+    mix.copy(['node_modules/bootstrap-sass/assets/fonts',
+              'vendor/fortawesome/font-awesome/fonts'
+             ], 'public/fonts');
+
+    //jquery ui themes
+    mix.copy('./vendor/components/jqueryui/themes/base', 'public/css/jqueryui_themes');
+
+    mix.webpack('app.js');
 });
--- resources/views/partials/foot.blade.php
+++ resources/views/partials/foot.blade.php
@@ -1,8 +1,5 @@
 
+        <script src="../../../js/all.js" type="text/javascript"></script>
         <script src="../../../js/app.js" type="text/javascript"></script>
-        <script src="../../../js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="../../../js/jquery.min.js" type="text/javascript"></script>
-        <script src="../../../js/jquery-ui.min.js" type="text/javascript"></script>
-        <script src="../../../js/main.js" type="text/javascript"></script>
     </body>
 </html>
