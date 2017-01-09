4a47dede6fa1943f0165dbadd0d5a79709d2113b
Brett Wilson <brettwilson7@yahoo.com>
2017-01-09 13:08:20 -0500

    Models - timestamps set to false.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,5 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model {
+    //I set timestamps to false because on update eloquent was using a different timezone then on create.
+    public $timestamps = false;
     protected $fillable = ['firstname','lastname','username','password'];
 }
